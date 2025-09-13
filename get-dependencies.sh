6d23d16dfce175849615146750804d6ed960885b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-13 05:09:52 -0400

    Create get-dependencies.sh

---
+++ get-dependencies.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl --prefer-nano gtk3-mini libxml2-mini opus-mini
+
+echo "Building linuxtoys..."
+echo "---------------------------------------------------------------"
+sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i 's|-O2|-O3|; s|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
+git clone https://aur.archlinux.org/linuxtoys-bin.git ./linuxtoys
+cd ./linuxtoys
+makepkg -fs --noconfirm
+ls -la .
+pacman --noconfirm -U ./*.pkg.tar.*
+
+pacman -Q linuxtoys-bin | awk '{print $2; exit}' > ~/version
